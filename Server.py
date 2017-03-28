3d1e3d1deb8f0c6d37de688b9c67de20630a0761
gongwen <1798550470@qq.com>
2017-03-28 23:08:51 +0800

    update

--- Server.py
+++ Server.py
@@ -4,21 +4,15 @@
 from flask import Flask 
 from flask import request
 from UserUtil import get_user_info
-from DbUtil import *
 
 import json
 import SearchLogic
-
-username = ''
-password = ''
-db = DbUtil()
 
 app = Flask(__name__)
 
 # 判断登录是否有效
 @app.route('/login', methods=['POST','GET'])
 def login():
-	global username,password
 	username = request.args.get('username')
 	password = request.args.get('password')
 	return json.dumps(get_user_info(username,password,False))
@@ -26,21 +20,26 @@ def login():
 # 搜索分裂标题
 @app.route('/search', methods=['POST','GET'])
 def search():
-	global username,password
-	result = get_user_info(username,password,True)
-	if result['status'] == False:
-		return json.dumps(result)
-
+	username = ''
+	password = ''
 	key_word = ''
 	number = 1
 	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
 		key_word = request.form['keyword']
 		number = request.form['pagesize']
 	elif request.method == 'GET':
+		username = request.args.get('username')
+		password = request.args.get('password')
 		key_word = request.args.get('keyword')
 		number = request.args.get('pagesize')
+		print(request.args)
 	else:
 		return json.dumps(())
+	result = get_user_info(username,password,True)
+	if result['status'] == False:
+		return json.dumps(result)
 	brand_list = SearchLogic.get_brand_list(key_word)
 	results = SearchLogic.get_processed_title(brand_list,SearchLogic.get_title_list_by_total_page(key_word,number))
 	result['list'] = results
